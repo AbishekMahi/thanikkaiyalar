90249374cb114b2d61baf27b802e88a49d25a4c2
Abishek Mahi <abishekmas0708@gmail.com>
2023-07-10 12:49:20 +0530

    meta tags and banner img add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Thanikkaiyalar
-* Updated: May 30 2023 with Bootstrap v5.3.0
-* Template URL: https://bootstrapmade.com/Thanikkaiyalar-free-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
