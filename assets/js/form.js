8d81df49ec720fe14fe0c18a7360a8210a180d4d
Abishek Mahi <abishekmas0708@gmail.com>
2023-10-16 23:37:47 +0530

    minor change

--- assets/js/form.js
+++ assets/js/form.js
@@ -111,3 +111,50 @@
         }
     }
 })();
+
+  // browser details, deviceType and url details
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var urlInput = document.getElementById("geturl");
+    urlInput.value = window.location.href;
+});
+
+// Detect the user's browser and get the browser name
+var browserName = detectBrowser();
+
+// Set the value of the input field with the browser name
+document.getElementById("browserName").value = browserName;
+
+function detectBrowser() {
+    var userAgent = navigator.userAgent;
+    var browserName = "Unknown";
+
+    if (userAgent.indexOf("Chrome") != -1) {
+        browserName = "Google Chrome";
+    } else if (userAgent.indexOf("Firefox") != -1) {
+        browserName = "Mozilla Firefox";
+    } else if (userAgent.indexOf("Safari") != -1) {
+        browserName = "Apple Safari";
+    } else if (userAgent.indexOf("MSIE") != -1 || userAgent.indexOf("Trident") != -1) {
+        browserName = "Internet Explorer";
+    } else if (userAgent.indexOf("Edge") != -1) {
+        browserName = "Microsoft Edge";
+    } else if (userAgent.indexOf("Opera") != -1 || userAgent.indexOf("OPR") != -1) {
+        browserName = "Opera";
+    }
+    return browserName;
+}
+
+// Detect the user's device type and get the device type
+var deviceType = detectDeviceType();
+
+// Set the value of the input field with the device type
+document.getElementById("deviceType").value = deviceType;
+
+function detectDeviceType() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return "Mobile Device";
+    } else {
+        return "Desktop Device";
+    }
+} 
